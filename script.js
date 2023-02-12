c5b945d5c0125a935cc6efd9b717497b90997968
lizagna <46845400+lizagna@users.noreply.github.com>
2023-02-11 23:29:43 -0600

    completed

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// var macQuantity = 0;
-// var pastaQuantity = 0;
-// var tacoQuantity = 0;
-// var enchiladaQuantity = 0;
-// var subtotal = 0;
-
 var data = {
     mac: {
         id: 1001,
@@ -32,8 +26,36 @@ var data = {
     subtotal: 0.0
 };
 
-$(document).ready(function () {});
+var foodNames = ["mac", "pasta", "tacos", "enchilada"];
 
+/**
+ * Handles modal display modes
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the modal
+    var orderPlacedModal = document.getElementById("order-confirmation");
+
+    var btnClose = document.getElementById("btnClose");
+
+    // When the user clicks on OK, close the modal
+    btnClose.onclick = function () {
+        orderPlacedModal.style.display = "none";
+        clearAll();
+    }
+
+    // When the user clicks anywhere outside of the modal, close it but still keep data
+    window.onclick = function (event) {
+        if (event.target == orderPlacedModal) {
+            orderPlacedModal.style.display = "none";
+        }
+    }
+});
+
+/**
+ * When the button is clicked it removes 1 item from the respective food item
+ * and updates the HTML value and the JSON object
+ * @param {*} input array that stores the food name and quantity id
+ */
 function onClickRemove(input) {
     let foodName = input[0];
     let quantityId = input[1];
@@ -51,6 +73,11 @@ function onClickRemove(input) {
     calculateSubtotal(data[foodName]);
 }
 
+/**
+ * When the button is clicked it adds 1 item from the respective food item
+ * and updates the HTML value and the JSON object
+ * @param {*} input array that stores the food name and quantity id
+ */
 function onClickAdd(input) {
     let foodName = input[0];
     let quantityId = input[1];
@@ -64,21 +91,34 @@ function onClickAdd(input) {
 }
 
 /**
- * Computes the order total every t
+ * Computes the order total for the current state of the menu
  */
 function calculateSubtotal() {
     subtotal = 0;
-    let foodNames = ["mac", "pasta", "tacos", "enchilada"];
-    
-    foodNames.forEach(function(foodName) {
+
+    foodNames.forEach(function (foodName) {
         var foodItem = data[foodName];
         let value = foodItem.price * foodItem.orderQuantity;
         subtotal += value;
     });
 
-    document.getElementById("order-subtotal").innerHTML = "$" + subtotal;
-
+    document.getElementById("order-subtotal").innerHTML = "Subtotal $" + subtotal;
 }
+
+
+// function setOrderBtnVisibility(subtotal) {
+//     var btnGrp = document.getElementsByClassName("order-btn-grp");
+
+//     for (var i = 0; i < btnGrp.length; i++)
+//     {
+//         if (subtotal > 0) {
+//             btnGrp[i].style.display = "block";
+//         } else {
+//             btnGrp[i].style.display = "none";
+//         }
+
+//     };
+// }
 
 /**
  * Not used but another option to calculate the subtotal
@@ -87,7 +127,7 @@ function calculateSubtotal() {
  */
 function updateSubtotal(foodItem, operationMethod) {
     let value = foodItem.orderQuantity * foodItem.price;
-    
+
     if (operationMethod == "+") {
         subtotal += value;
     } else {
@@ -98,19 +138,16 @@ function updateSubtotal(foodItem, operationMethod) {
 }
 
 /**
- * Resets all the food item quantity values back to 0 and 
- * the subtotal back to 0
+ * Resets all the food item quantity values back to 0 and the subtotal back to 0
  */
 function clearAll() {
     subtotal = 0;
-    let foodNames = ["mac", "pasta", "tacos", "enchilada"];
-
     /**
      * Goes through each food item and zeros out their quantities
      */
-    foodNames.forEach(function(foodName) {
+    foodNames.forEach(function (foodName) {
         let quantityId = foodName + "-quantity";
-        
+
         data[foodName].orderQuantity = 0;
 
         let element = document.getElementById(quantityId);
@@ -119,11 +156,28 @@ function clearAll() {
     });
 
     calculateSubtotal();
-
-    // document.getElementById("order-subtotal").innerHTML = "$" + subtotal;
-  
 }
 
+/**
+ * Enables the modal to pop-up modal and displays the item and quantity of items ordered
+ */
 function submitOrder() {
+    let orderedItems = "";
+    foodNames.forEach(function (foodName) {
+        let itemQuantity = data[foodName].orderQuantity;
+        if (itemQuantity > 0) {
+            let itemName = data[foodName].name;
+            orderedItems += "" + itemQuantity + " " + itemName + " ";
+        }
+    });
     
+    let confirmationTitle = "Order placed!";
+    if (orderedItems === "") {
+        confirmationTitle = "No items in cart";
+    } 
+    document.getElementById("order-confirmation-title").innerHTML = confirmationTitle;
+        
+    document.getElementById("confirmed-items").innerHTML = orderedItems;
+    var orderPlacedModal = document.getElementById("order-confirmation");
+    orderPlacedModal.style.display = "block";
 }
